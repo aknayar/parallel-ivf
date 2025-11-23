519b66c4b2cd24cdb1c99978b35365e830f0381e
Dhruva J Byrapatna <dbyrapat@ghc86.ghc.andrew.cmu.edu>
2025-11-22 19:10:30 -0500

    I am an idiot

--- tests/utils.py
+++ tests/utils.py
@@ -14,17 +14,17 @@ def get_all_indexes(d, nlist):
 def getIndex(name, d, nlist):
     indexes = []
     
-    if "IVFBase" in name:
+    if "IVFBase" == name:
         indexes.append(parallel_ivf.IVFBase(d=d, nlist=nlist))
-    if "IVFSIMD" in name:
+    if "IVFSIMD" == name:
         indexes.append(parallel_ivf.IVFSIMD(d=d, nlist=nlist))
-    if "IVFSIMDQueryParallel" in name:
+    if "IVFSIMDQueryParallel" == name:
         indexes.append(parallel_ivf.IVFSIMDQueryParallel(d=d, nlist=nlist))
-    if "IVFSIMDCandidateParallel" in name:
+    if "IVFSIMDCandidateParallel" == name:
         indexes.append(parallel_ivf.IVFSIMDCandidateParallel(d=d, nlist=nlist))
-    if "IVFScalarQueryParallel" in name:
+    if "IVFScalarQueryParallel" == name:
         indexes.append(parallel_ivf.IVFScalarQueryParallel(d=d, nlist=nlist))
-    if "IVFScalarCandidateParallel" in name:
+    if "IVFScalarCandidateParallel" == name:
         indexes.append(parallel_ivf.IVFScalarCandidateParallel(d=d, nlist=nlist))
     return indexes
 
